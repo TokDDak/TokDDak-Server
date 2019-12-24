fa38bfb63b7f11ec5f9789faec7e712ef9e72283
Hyun-juhee <juhd1998@gmail.com>
2019-12-24 23:43:19 +0900

    Transportation controller, service 1차구현

---
+++ Transportation/controller.js
@@ -0,0 +1,96 @@
+const TransService = require('./service');
+const rm = require('../module/util/responseMessage');
+const utils = require('../module/util/utils');
+const sc = require('../module/util/statusCode');
+
+module.exports = {
+    readAll: async (req, res) => {
+        TransService.readAll()
+        .then(({
+            json
+        }) => 
+            res.send(json)
+        ).catch(err => {
+            console.log(err);
+            res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+        });
+    },
+    readOne: async (req, res) => {
+        const {
+            id
+        } = req.body;
+        if(!id) {
+            res.send(utils.successFalse(sc.BAD_REQUEST, rm.NULL_VALUE));
+            return;
+        }
+        TransService.readOne({id})
+        .then(({
+            json
+        }) => 
+            res.send(json)
+        ).catch(err => {
+            console.log(err);
+            res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+        })
+    },
+    create: async (req, res) => {
+        const {
+            
+            // ??
+        } = req.body;
+        if(!host) {
+            res.send(utils.successFalse(sc.BAD_REQUEST, rm.NULL_VALUE));
+            return;
+        }
+        TransService.create({host, introduce})
+        .then(({
+            json
+        }) => 
+            res.send(json)
+        ).catch(err => {
+            res.send(err);
+        })
+    },
+    update: async (req, res) => {
+        const {
+            id,
+            introduce
+        } = req.body;
+        if (!id || !introduce) {
+            const missParameters = Object.entries({
+                id,
+                    introduce
+                })
+                .filter(it => it[1] == undefined).map(it => it[0]).join(',');
+            res.send(utils.successFalse(sc.BAD_REQUEST, `${rm.NULL_VALUE}, ${missParameters}`));
+            return;
+        }
+        TransService.update({id, introduce})
+        .then(({
+            json
+        }) => 
+            res.send(json)
+        ).catch(err => {
+            console.log(err);
+            res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+        })
+    },
+    delete: async (req, res) => {
+        const {
+            id
+        } = req.body;
+        if(!id) {
+            res.send(utils.successFalse(sc.BAD_REQUEST, rm.NULL_VALUE));
+            return;
+        }
+        TransService.delete({blogId})
+        .then(({
+            json
+        }) => 
+            res.send(json)
+        ).catch(err => {
+            console.log(err);
+            res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+        })
+    },
+}
---
+++ Transportation/service.js
@@ -0,0 +1,118 @@
+const rm = require('../module/util/responseMessage');
+const utils = require('../module/util/utils');
+const sc = require('../module/util/statusCode');
+
+
+const {Blog, Article, Comment} = require('../models/transportationModel');
+
+module.exports = {
+    readAll: () => {
+        return new Promise(async (resolve, reject) => {
+            const blog = await Blog.findAll({});
+            if(blog.length == 0) {
+                resolve({
+                    json: utils.successFalse(sc.NO_CONTENT, rm.BLOG_EMPTY)
+                });
+                return;
+            }
+            if (!blog) {
+                resolve({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_READ_ALL_FAIL)
+                });
+                return;
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_READ_ALL_SUCCESS, blog)
+            });
+        });
+    },
+    readOne: ({
+        id
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            const blog = await Blog.findOne({
+                where: {
+                    id : id,
+                }
+            });
+            if(blog.length == 0) {
+                resolve({
+                    json: utils.successFalse(sc.NO_CONTENT, rm.BLOG_READ_BLOGIDX_FAIL)
+                });
+                return;
+            }
+            if (!blog) {
+                resolve({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_READ_BLOGIDX_FAIL)
+                });
+                return;
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_READ_BLOGIDX_SUCCESS, blog)
+            });
+        });
+    },
+    create: ({
+        host,
+        introduce
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            let blog;
+            try {
+                blog = await Blog.create({
+                        host: host,
+                        introduce: introduce,
+                });
+                
+            } catch (error) {
+                reject({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_CREATE_FAIL)
+                });
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_CREATE_SUCCESS, blog)
+            });
+        });
+    },
+    update: ({
+        blogIdx,
+        introduce
+    }) => {
+        return new Promise(async (resolve, reject) => {
+            const blog = await Blog.update({
+                introduce : introduce
+            }, {
+                where : { blogIdx : blogIdx },
+            });
+            if (!blog) {
+                resolve({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_UPDATE_FAIL)
+                });
+                return;
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_UPDATE_SUCCESS)
+            });
+        });
+    },
+    delete: ({blogIdx}) => {
+        return new Promise(async (resolve, reject) => {
+            let blog;
+            try {
+                blog = await Blog.destroy({
+                    where:{
+                        blogIdx: blogIdx
+                    }
+                });
+                
+            } catch (error) {
+                reject({
+                    json: utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.BLOG_DELETE_FAIL)
+                });
+            }
+            resolve({
+                json: utils.successTrue(sc.SUCCESS, rm.BLOG_DELETE_SUCCESS)
+            });
+        });
+    },
+};
