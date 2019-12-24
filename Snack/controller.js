66b39d204c2f55cf59a998e0ebc658e0a5aae972
toneyparky <qkrtmddhks95@gmail.com>
2019-12-24 23:40:02 +0900

    Sanck 컨트롤러, 서비스 초기 구현

---
+++ Snack/controller.js
@@ -0,0 +1,107 @@
+const SnackService = require('./service');
+
+const rm = require('../module/util/responseMessage');
+const utils = require('../module/util/utils');
+const sc = require('../module/util/statusCode');
+
+module.exports = {
+    readAll: async (req, res) => {
+        SnackService.readAll()
+            .then(({
+                    json
+                }) =>
+                res.send(json)
+            ).catch(err => {
+                console.log(err);
+                res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+            });
+    },
+    readOne: async (req, res) => {
+        const {
+            id
+        } = req.body;
+        if (!id) {
+            res.send(utils.successFalse(sc.BAD_REQUEST, rm.NULL_VALUE));
+            return;
+        }
+        SnackService.readOne({
+                id
+            })
+            .then(({
+                    json
+                }) =>
+                res.send(json)
+            ).catch(err => {
+                console.log(err);
+                res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+            })
+    },
+    create: async (req, res) => {
+        const {
+            host, // 추후 수정
+            introduce // 추후 수정
+        } = req.body;
+        if (!host) {
+            res.send(utils.successFalse(sc.BAD_REQUEST, rm.NULL_VALUE));
+            return;
+        }
+        SnackService.create({
+                host,
+                introduce
+            })
+            .then(({
+                    json
+                }) =>
+                res.send(json)
+            ).catch(err => {
+                res.send(err);
+            })
+    },
+    update: async (req, res) => {
+        const {
+            id,
+            introduce
+        } = req.body;
+        if (!id || !introduce) {
+            const missParameters = Object.entries({
+                    id,
+                    introduce
+                })
+                .filter(it => it[1] == undefined).map(it => it[0]).join(',');
+            res.send(utils.successFalse(sc.BAD_REQUEST, `${rm.NULL_VALUE}, ${missParameters}`));
+            return;
+        }
+        SnackService.update({
+                id,
+                introduce
+            })
+            .then(({
+                    json
+                }) =>
+                res.send(json)
+            ).catch(err => {
+                console.log(err);
+                res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+            })
+    },
+    delete: async (req, res) => {
+        const {
+            id
+        } = req.body;
+        if (!id) {
+            res.send(utils.successFalse(sc.BAD_REQUEST, rm.NULL_VALUE));
+            return;
+        }
+        SnackService.delete({
+                blogId
+            })
+            .then(({
+                    json
+                }) =>
+                res.send(json)
+            ).catch(err => {
+                console.log(err);
+                res.send(utils.successFalse(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+            })
+    },
+}
