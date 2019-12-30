dbf23d0f0f139d9918b3b9c9ad36e40e13e53495
kkoon9 <rndrnjs2003@naver.com>
2019-12-30 16:51:56 +0900

    median Model 최종 수정

--- models/medianModel.js
+++ models/medianModel.js
@@ -1,15 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('Median', {
-        hotel: { 
+        cityId: {
             type: DataTypes.INTEGER,
             allowNull: false,
+        },
+        category: {
+            type: DataTypes.STRING(30),
+            allowNull:false,
+        },
+        hotel: { 
+            type: DataTypes.INTEGER,
+            allowNull: true,
         },
         food: { // 금액
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
         },
     }, {
         freezeTableName: true,
-        timestamps: true,
+        timestamps: false,
     });
 };
