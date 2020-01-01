f354b65cedf0bb6adde1d35056bb64da159b47a1
kkoon9 <rndrnjs2003@naver.com>
2020-01-02 00:52:24 +0900

    median, random hotel 완료

--- module/median.js
+++ module/median.js
@@ -3,14 +3,20 @@ const {
 } = require('../models');
 const Random = require('../module/random');
 const Op = require('sequelize').Op;
+
+const promise = async (json, start, result) => {
+    result[start/3].info = json.slice(start, start+3);
+}
+
 module.exports = {
     hotelRead: async ({
         cityId,
     }) => {
         return new Promise(async (resolve, reject) => {
             const result = [];
+            const infoArr = [];
             let subCategory = 2;
-            const obj = new Object();
+            const CityId = cityId;
             //            const categoryArr = ['저가호텔', '일반호텔', '고급호텔', '최고급호텔'];
             try {
                 const test = await Median.findAll({
@@ -22,30 +28,28 @@ module.exports = {
                     },
                     attributes: ['cityId', 'category', 'cost', 'urlHotel'],
                 });
-                
-                await test.forEach(async (buffer) => {
-                    
-                    const bmp = buffer.dataValues;
-                    const CityId = bmp.cityId;
+                for (let i = 0; i < 4; i++) {
+                    const bmp = test[i].dataValues;
+                    const obj = new Object();
                     obj.category = bmp.category;
                     obj.cost = bmp.cost;
-                    await Random.randomHotel({
-                            CityId,
-                            subCategory
+                    obj.url = bmp.urlHotel
+                    obj.info = [];
+                    result.push(obj);
+                }
+                await Random.randomHotel()
+                    .then(async ({
+                        json
+                    }) => {
+                        Promise.all([promise(json, 0, result), promise(json, 3, result), promise(json, 6, result), promise(json, 9, result)]).then(function () {
+                            console.log(result[1].info);
+                            resolve({
+                                result
+                            })
                         })
-                        .then(({
-                            json
-                        }) => {
-                            obj.info = json;
-                            subCategory += 1;
-                            obj.url = bmp.urlHotel
-                            result.push(obj);
-                        }).catch(err =>
-                            console.log(err))
-                })
-                resolve({
-                    result
-                });
+
+                    }).catch(err =>
+                        console.log(err))
             } catch (err) {
                 console.log(err);
                 reject(err);
--- module/random.js
+++ module/random.js
@@ -1,79 +1,71 @@
-const { Food, Hotel } = require('../models');
+const {
+    Food,
+    Hotel
+} = require('../models');
 
 module.exports = {
-    randomFood : async ({grade,CityId})=> { //grade 1,2,3은 내가 넣는대로 
-        return new Promise(async(resolve, reject)=>{
+    randomFood: async ({
+        grade,
+        CityId
+    }) => { //grade 1,2,3은 내가 넣는대로 
+        return new Promise(async (resolve, reject) => {
             const data = [];
-            try{
+            try {
                 foodbyGrade = await Food.findAll({
-                    where : {
-                        grade : grade,
-                        CityId : CityId
-                    },
-                    attributes: ['id', 'name', 'grade', 'cost'],
-                });
-<<<<<<< HEAD
-                console.log(foodbyGrade.length);
-                var arrId = new Array();
-                for(var i=0; i<foodbyGrade.length; i++) {
-                    arrId[i] = foodbyGrade[i].id;
-                }
-              
-                for(var i=0; i<3 ; i++)
-                {
-                    random = Math.floor(Math.random()*(foodbyGrade.length - 0));
-                    //data[i] = grade1[random];
-                    for(var j =0;j<foodbyGrade.length; j++)
-                    {
-                        if(foodbyGrade[j].id == arrId[random])
-                        {
-                            data[i] = foodbyGrade[j];
-                        }
-                    }
-=======
-                const len = foodbyGrade.length; 
-                console.log("len : ", len);             
-                for(let i=0; i<3 ; i++)
-                {
-                    random = Math.floor(Math.random()*(len - 0));
-                    data[i] = foodbyGrade[random].dataValues;
->>>>>>> func/median
-                }
-                resolve({
-                    json : data
-                })
-            }catch(err){
-                console.log(err);
-                reject(err);
-            }
-        })
-    },
-    
-    randomHotel : async({subCategory,CityId})=> { //subCategory
-        return new Promise(async(resolve, reject)=>{
-            const data = [];
-            try{
-                cate = await Hotel.findAll({
-                    where : {
-                        subCategory : subCategory,
-                        CityId : CityId
+                    where: {
+                        grade: grade,
+                        CityId: CityId
                     },
                     attributes: ['name', 'cost'],
                 });
-              
-                for(let i=0; i<3 ; i++)
-                {
-                    random = Math.floor(Math.random()*(cate.length - 0));
-                    data[i] = cate[random].dataValues;;
+                const len = foodbyGrade.length;
+                for (let i = 0; i < 3; i++) {
+                    random = Math.floor(Math.random() * (len - 0));
+                    data[i] = foodbyGrade[random].dataValues;
                 }
                 resolve({
-                    json : data
+                    json: data
                 })
-            }catch(err){
+            } catch (err) {
                 console.log(err);
                 reject(err);
             }
         })
     },
-   
+
+    randomHotel: async () => { //subCategory
+        return new Promise(async (resolve, reject) => {
+            const data = [];
+            let i = 0;
+            let j
+            try {
+                const promise = async (
+                    subCategory,
+                    CityId
+                ) => {
+                    cate = await Hotel.findAll({
+                        where: {
+                            subCategory: subCategory,
+                            CityId: CityId
+                        },
+                        attributes: ['name', 'cost'],
+                    });
+                    const start = (subCategory - 2) * 3;
+                    for (i = start; i < start + 3; i++) {
+                        random = Math.floor(Math.random() * (cate.length - 0));
+                        data[i] = cate[random].dataValues;;
+                    }
+                }
+                Promise.all([promise(2, 1), promise(3, 1), promise(4, 1), promise(5, 1)]).then(function () {
+                    resolve({
+                        json: data
+                    })
+                })
+
+            } catch (err) {
+                console.log(err);
+                reject(err);
+            }
+        })
+    },
 }
